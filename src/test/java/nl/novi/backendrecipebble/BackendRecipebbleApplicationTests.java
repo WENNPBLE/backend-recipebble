6b26dadff19d983c05a9c588a56ce736e4a6dd60
wennpble <smitswen@gmail.com>
2022-07-07 10:55:19 +0200

    started a new project

---
+++ src/test/java/nl/novi/backendrecipebble/BackendRecipebbleApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.backendrecipebble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRecipebbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
